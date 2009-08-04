2aa8701df6a0cc4c2c0c267ba9e295db6cf1adf6
joaobispo <joaobispo@6bb3e47e-7e01-11de-aea3-ffc7f6571d4c>
2009-08-04 11:01:06 +0000

    -Removed unit test for MbAdd; - Added interface MbInstruction; - Switched MbAdd from Runnable to MbInstruction;

--- trunk/projects/MbTraceInterpreter/trunk/src/org/ancora/MicroblazeInterpreter/Instructions/MbAdd.java
+++ trunk/projects/MbTraceInterpreter/trunk/src/org/ancora/MicroblazeInterpreter/Instructions/MbAdd.java
@@ -26,7 +26,7 @@ import org.ancora.MicroblazeInterpreter.Parser.TraceData;
  *
  * @author Joao Bispo
  */
-public class MbAdd implements Runnable {
+public class MbAdd implements MbInstruction {
 
     public MbAdd(TraceData data) {
         // Parse the data
@@ -70,7 +70,7 @@ public class MbAdd implements Runnable {
     /**
      * Executes the instruction
      */
-    public void run() {
+    public void execute() {
         // If cBit, get carry from special register file
         int carry = 0;
         if(cBit) {
@@ -92,8 +92,18 @@ public class MbAdd implements Runnable {
             int carryOut = Extra.getCarryOut(rA, rB, carry);
             // SPR <- CarryOut
         }
-        
+
+        // Advance CycleCount
+        // CycleCount.next(latency)
     }
+
+   public int latency() {
+      return LATENCY;
+   }
+
+   public boolean isBranch() {
+      return IS_BRANCH;
+   }
 
     @Override
     public String toString() {
@@ -107,30 +117,6 @@ public class MbAdd implements Runnable {
         
         return builder.toString();
     }
-
-
-    // Getters for Unit Tests
-    public boolean iscBit() {
-        return cBit;
-    }
-
-    public boolean iskBit() {
-        return kBit;
-    }
-
-    public int getRegA() {
-        return regA;
-    }
-
-    public int getRegD() {
-        return regD;
-    }
-
-    public int getRegB() {
-        return regB;
-    }
-
-
 
 
     // INSTANCE VARIABLES
@@ -150,4 +136,8 @@ public class MbAdd implements Runnable {
     private final String C_CHAR = "c";
     private final String K_CHAR = "k";
     private final int BEGIN_INDEX_REG = 1;
+    private final int LATENCY = 1;
+    private final boolean IS_BRANCH = false;
+
+
 }
